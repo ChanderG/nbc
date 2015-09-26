d1ba5496548179ceab060f6ed83e9e47f69f8cf0
ChanderG <chandergovind@gmail.com>
2015-09-26 11:36:12 +0530

    add gaussian method

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #! /usr/bin/python
 
+import math
+import numpy as np
+
 def loadData(filename):
     """Load data from filename and return in dict format."""
     rawdata = open(filename).read()
@@ -15,6 +18,11 @@ def loadData(filename):
             d[kv[0]] = kv[1]
         data.append(d)
     return data
+
+def calculateProbability(x, mean, stdev):
+    """ Gaussian distribution. """
+    exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
+    return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
 
 def createNBClassifier(data):
     """ Create a Naive Bayes classifier. 
@@ -53,12 +61,39 @@ def createNBClassifier(data):
             else:
                 probset[d[k]] = probset.get(d[k], 0) + 0
 
-        #print "Feature ", k, " has ", len(probset.keys())
-        #if len(probset.keys()) < 
+        # arbitrary cutoff to decide when the number of keys are too many
+        if len(probset.keys()) > 0.3*len(data):
+            # too many keys present
+            # discrete probability does not make sense
+            # we need to model a gaussian distribution
+            #probset = {}
+            probset['gaussian'] = True
 
-        # convert to probabilities
-        for p in probset.keys():
-            probset[p] = float(probset[p])/totalnos
+            # obtain mean and standard deviation
+            true_nos = []
+            false_nos = []
+            for d in data:
+                if d['class'] == true_label:
+                    true_nos.append(float(d[k]))
+                else:
+                    false_nos.append(float(d[k]))
+            
+            true_nos = np.array(true_nos)
+            false_nos = np.array(false_nos)
+
+            probset['true_mean'] = float(np.mean(true_nos))
+            probset['true_std'] = float(np.std(true_nos))
+
+            probset['false_mean'] = float(np.mean(false_nos))
+            probset['false_std'] = float(np.std(false_nos))
+
+        else: 
+            # use ordinary distribution
+            probset['gaussian'] = False
+
+            # convert to probabilities
+            for p in probset.keys():
+                probset[p] = float(probset[p])/totalnos
 
         # add it master dict
         classifier[k] = probset
@@ -80,8 +115,15 @@ def runNBClassifier(classifier, testdata):
     for k in testdata.keys():
        if k == 'class':
            continue
-       prob_true = prob_true * classifier[k][testdata[k]] 
-       prob_false = prob_false * (1.0 - classifier[k][testdata[k]])
+       if classifier[k]['gaussian'] == False:
+           prob_true = prob_true * classifier[k][testdata[k]] 
+           prob_false = prob_false * (1.0 - classifier[k][testdata[k]])
+
+       else:
+           # alternative method to calculate probability
+           prob_true = calculateProbability(float(testdata[k]), classifier[k]['true_mean'], classifier[k]['true_std'])
+           prob_false = calculateProbability(float(testdata[k]), classifier[k]['false_mean'], classifier[k]['false_std'])
+
 
     if prob_true > prob_false:
         return classifier['true']
